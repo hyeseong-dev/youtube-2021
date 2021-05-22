b07a315e78f36fddfeb20269e11f58519ba9d070
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 23:14:17 +0900

    Edit Video -2 I. post로 보내진 request의 body에 데이터를 해석하려면 미들웨어를 거쳐서 필수적인 정제 과정을 해야하는데요.   1. server.js에서 express.urlencoded메소드를 통해서 extended: true 옵션을 설정해주어야합니다.   그렇게 해야 postEdit 컨트롤러에서 req.body에 접근하여 title 변수에 받아온 값을 할당해 줄 수 있습니다.   이후 변수에 할당한 값은 fakeDB인 videos객체의 title속성에 접근하여 가져온 title값을 재할당하고 마무리로 return키워드에는 res.redirect()를 사용하여 매개변수로 /vides/${id}로 받아 처리합니다.   2. 특히 라우터에서는 중복된 코드를 재활용하고 단축하기 위해 라우터 객체에 route()메소드를 URL 정의를 1차로 해주고 이어서 .get(컨트롤러함수).post(컨트롤러함수)를 작성하여 한줄안에 동일한 URL자원정의를 활용할수 있습니다.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,7 +10,9 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
+// videoRouter.get("/:id(\\d+)", watch);
+// videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
