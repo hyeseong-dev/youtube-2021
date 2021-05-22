c63573513377bc1d5ff27c51c6c612b1cdb21f13
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 21:54:26 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,28 @@
 export const trending = (req, res) => {
   const videos = [
     {
-      title: "Hello",
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
-      title: "Video #2",
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
-      title: "Whatsup",
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
   ];
   const nideos = [1,2,3,4,5,6,7,8,9,10]
