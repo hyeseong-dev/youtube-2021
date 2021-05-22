740fe0ff39cb7f09703fcad639de0f7547c97515
hyeseong-lee <hyeseong43@gmail.com>
2021-05-23 02:49:15 +0900

    Video Model I. model 생성을 위해선 스키마를 만듭니다. 뼈대입니다. "키:데이터타입"구조로 이루어집니다.  1. 여기서 쓰이는 데이터 타입은 String, Date, [], {}이 일단 쓰입니다.  2. mongoose.Schema에 구조를 정의하고 이를 videoSchema변수로 받습니다.  3. mongoose.model()메서드를 사용해서 첫 번째 매개변수에 Video를 모델이름으로 정의하고 두번째 매개변수에는 사전 정의한 스키마를 위치시킵니다. 그리고이를 변수인 Video로 할당하고 이를 export default 키워드로 외부에 참조 가능하도록 합니다.  4. videos모듈은 server모듈에서 임포트하여 사용가능하도록 합니다.

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb//127.0.0.1:27017/youtube",{   //rooturl 슬래시 이후에는 db를 자동으로 생성해주며 있는 경우 접속을 뜻함
-    useNewUrlParser: true,
-    useUnifieldTopology: true,
+mongoose.connect("mongodb://127.0.0.1:27017/youtube",{   //rooturl 슬래시 이후에는 db를 자동으로 생성해주며 있는 경우 접속을 뜻함
+    useNewUrlParser: true,       // To use the new parser,to MongoClient.js driver rewrote the tool it uses to parse MongoDB connection string
+    useUnifiedTopology: true   // mongoose의 드라이버와 MongoDB의 드라이버는 같은 소켓에서 데이터가 왔다갔다 할 수 있다는 점
+    
 });
 
 const db = mongoose.connection;
@@ -10,5 +11,5 @@ const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❌ Connected to DB");
 
-db.on("error", handleError);
-db.once("open", handleOpen);
+db.on("error", handleError); // db연결 실패시 에러 메시지 출력
+db.once("open", handleOpen); // 디비 연결 성공시 메시지 출력
