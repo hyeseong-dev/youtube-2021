6e679ae403ab7a8d7fd1ec3f5ae5cfea18babef8
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 19:43:27 +0900

    MVP styles

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globaRouter";
-
 const PORT = 4000;
-
+console.log(process.cwd());
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
