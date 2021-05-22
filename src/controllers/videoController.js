a0137d0b93a2bf329db6b5b81dd7ebaebd22ed5c
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 21:37:37 +0900

    Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,18 @@
-const fakeUser = {
-  username: 'hyeseong',
-  loggedIn: true,
-}
-
-export const trending = (req, res) => res.render("home", { fakeUser });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  const nideos = [1,2,3,4,5,6,7,8,9,10]
+  return res.render("home", { pageTitle: "Home", videos, nideos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
