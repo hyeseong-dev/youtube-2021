faf9886c81a614be681d6c43cb17e40188716ae7
hyeseong-lee <hyeseong43@gmail.com>
2021-05-23 21:07:10 +0900

    Search - 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/video"
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});  
+  const videos = await Video.find({}).sort({ createdAt: "desc" });  
   return res.render("home", { pageTitle: "Home", videos });
 }
 
@@ -60,10 +60,18 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
-}
+};
 
 export const deleteVideo = async (req, res) =>{
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+    //search
+  }
+  return res.render("search", { pageTitle: "Search "})
 }
