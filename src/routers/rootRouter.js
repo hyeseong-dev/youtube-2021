b1808053418a294e5fe5776c5a08b29e395ba93a
hyeseong-lee <hyeseong43@gmail.com>
2021-05-24 22:27:06 +0900

    Logged In User postLogin 컨트롤러에 버그가 있는 상태 이를 해결하려면 변경된 req객체를 템플릿에 넘겨주어야 하는 것으로 파악됨

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { getJoin, getLogin, postJoin, postLogin, } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
-
 
 const rootRouter = express.Router();
 
@@ -10,4 +14,4 @@ rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
-export default rootRouter
+export default rootRouter;
--- src/server.js
+++ src/server.js
@@ -15,23 +15,17 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(
     session({
-        secret: "Helloffffffffffffffffffffffffffffffasdfasdfasdfffffff!",
+        secret: "Hello!",
         resave: true,      //  false: 세션 데이터가 바뀌기 전까지는 세션 저장소에 값을 저장하지 않음  // true: 세션 데이터가 바뀌던 안바뀌던 세션 저장소에 값을 계속 저장을 함
         saveUninitialized:true,
     })
 );
 
-app.use((req, res, next) =>{     // session 미들웨어
-    req.sessionStore.all((error, sessions) => {
-        console.log('===asdfasdfasdfasdfsdf', );
-        next();
-    })
-})
-
-app.get("/add-one", (req, res, next) => {
-    req.session.potato += 1;
-    req.session.fuck += 1;
-    return res.send(`${req.session.id}==${req.session.potato}==${req.session.fuck}`);
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
 });
 
 app.use("/", rootRouter);
