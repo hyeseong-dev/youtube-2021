19d695240cb73706c9f955f4db9a9a498083f24e
hyeseong-lee <hyeseong43@gmail.com>
2021-05-25 14:01:46 +0900

    Expiration and Secrets & Environment Variables

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/youtube",{   //rooturl 슬래시 이후에는 db를 자동으로 생성해주며 있는 경우 접속을 뜻함
+mongoose.connect(process.env.DB_URL,{   //rooturl 슬래시 이후에는 db를 자동으로 생성해주며 있는 경우 접속을 뜻함
     useNewUrlParser: true,       // To use the new parser,to MongoClient.js driver rewrote the tool it uses to parse MongoDB connection string
     useUnifiedTopology: true,   // mongoose의 드라이버와 MongoDB의 드라이버는 같은 소켓에서 데이터가 왔다갔다 할 수 있다는 점
     useFindAndModify: false,    // true by default. Set to false to make findOneAndUpdate() and findOneAndRemove() use native findOneAndUpdate() rather than findAndModify().
--- src/server.js
+++ src/server.js
@@ -17,12 +17,29 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(
     session({
-        secret: "Hello!", 
+        secret: process.env.COOKIE_SECRET, 
         resave: false,      //  false: 세션 데이터가 바뀌기 전까지는 세션 저장소에 값을 저장하지 않음  // true: 세션 데이터가 바뀌던 안바뀌던 세션 저장소에 값을 계속 저장을 함
         saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/youtube"}),
-      })
+        cookie: { maxAge: 1000 * 60 * 60 * 3 }, // session의 쿠키 만료 시간을 3시간으로 설정
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL}),
+      }),
 );
+
+app.use((req, res, next) => {
+  // console.log('request의 키들', Object.keys(req)),
+  // console.log('req.headers.cookie: ', req.headers.cookie )
+  // console.log('req.session :  ', req.session),
+  // console.log('req.session.cookie : ', req.session.cookie)
+  // console.log('req.sessionID :  ', req.sessionID),
+  // console.log('req.sessionStore :  ', req.sessionStore),
+    next();
+});
+  
+
+
+
+
+
 app.use(localsMiddleware) // 만약  localsMiddleware가 session 미들웨어보다 먼저 읽히면 session이 req객체에 저장되지 않음 즉, 현재 순서로 되어 있으면 localMiddleware가 session 객체에 접근 할 수 있음 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
