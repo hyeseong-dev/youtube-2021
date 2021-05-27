3e03f1a8679629acb2282651a2e7b485d9112f84
hyeseong-lee <hyeseong43@gmail.com>
2021-05-28 03:10:19 +0900

    Edit Profile POST - 1
    1. base템플릿에 Upload Video  a태그와  li태그를 만들어 줍니다.(로그인시)
    2. videoRouter에는 특정 유저 조회와 삭제를 제외하고는 다른 엔드포인트들은 route, all, get, post를 순차적으로 적용하고 관련 로직에 따라 미들웨어를 매개변수로 받습니다.
    3. 유저컨트롤러 로직에서는 유저 정보 변경을 위한 로직 처리를 구성합니다.
    3.1 `const { session: { user: { _id }, }, } = req` <---- 방법은 사실 const  i = req.session.user.id 동일합니다.
    3.2 `const { name, email, username, location } = req.body;`
    - 3.1, 3.2의 방식이 ES6이며 이는 리소스를 좀더 효율적으로 가져오는 것은 물론 가독성에도 도움이 됩니다. 실질적으로 `_id`, `name, email, username, location` 이 postEdit함수 스코프안에 변수가 생겼다고 보시면 됩니다.
    
    4. 하지만 이상태로는 세션이 업데이트 되지 않아 템플릿에는 변화되지 않은 것처럼 보입니다. 디비에서는 POST요청에 따른 정보를 받고 데이터 변경은 완료되었지만요.

--- src/middlewares.js
+++ src/middlewares.js
@@ -7,16 +7,16 @@ export const localsMiddleware = (req, res, next) => {
 
 export const protectorMiddleware = (req, res, next) => {
     if (req.session.loggedIn) {
-        return next();
-    }else{
-        return res.redirect("/login");
+      return next();
+    } else {
+      return res.redirect("/login");
     }
-};
-
-export const publicMiddleware = (req, res, next) =>{
-    if (!req.session.loggedIn) {
-        return next();
-    }else {
-        return res.redirect("/");
-    }
+  };
+  
+export const publicOnlyMiddleware = (req, res, next) => {
+if (!req.session.loggedIn) {
+    return next();
+} else {
+    return res.redirect("/");
+}
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,11 +6,11 @@ import {logout,
         getEdit,
         postEdit,
     } from "../controllers/userController";
-import { publicOnlyMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout",protectorMiddleware, logout);
+userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/github/start",publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
