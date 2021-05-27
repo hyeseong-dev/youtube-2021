a931404131b473f66c802de3c9b6bb94a73b1004
hyeseong-lee <hyeseong43@gmail.com>
2021-05-28 00:55:59 +0900

    Edit Profile GET
    1. edit-profile.pub 템플릿을 생성합니다.
     - name, email, username, location 입력 input태그를 만들고 submit 타입의 input도만들어 줍니다. submit 버튼은 String으로 Update Profile 문자열을 값을 value 속성으로 할당하고 나머지는 loggedInUser객체를 이용하여 input태그에 대응하는 속성에 접근하여 할당합니다.
    
    2. base 템플릿의 경우, "/users/edit" 속성 값을 할당하고 텍스트는 Edit Profile로 합니다.
    
    3. 라우트에는 getEdit, postEdit 컨트롤러를 URL과 맵핑합니다.
    
    4. 그럼 getEdit, postEdit컨트롤러에는 return시 render할 템플릿 매개변수와 페이지 타이틀정보가 있는 객체를 넣어 프런트로 보낼 채비를 합니다.
    
    5. localMiddleware에서는 req.session.user(비로그인유저)에 undefined와 같은 값이 할당 될 경우 주소창에 메뉴얼로 /users/edit/으로 접근할 경우 오류가 발생함으로 ||(or)연산자와 빈 객체 {} 를 이용하여 오류 처리를 합니다.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -112,6 +112,7 @@ export const finishGithubLogin = async (req, res) => {
       (email) => email.primary === true && email.verified === true
     );
     if (!emailObj) {
+      //set notification
       return res.redirect("/login");
     }
     let user = await User.findOne({ email: emailObj.email});
@@ -139,5 +140,13 @@ export const logout = (req, res) => {
   return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const see = (req, res) => res.send("See User");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
+
+export const see = (req, res) => {
+  return res.render("edit-profile");
+};
