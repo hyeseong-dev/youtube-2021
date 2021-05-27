2aeebe9ee74d3401a45fe8b58050a3d4b54f82ad
hyeseong-lee <hyeseong43@gmail.com>
2021-05-28 01:23:32 +0900

    Protector and Public Middlewares
    1. middleware에서 2가지를 추가로 정의합니다.
     - 로그인이 필요한 경우의 endpoint와 V.S. 로그인이 필요하지 않은 endpoint들로 분류
      - protoectorMiddleware로 함수를 선언합니다. 그리고 조건문으로 분기해요 이때 조건으로 `req.session.loggedIn`을 사용합니다. 그리고 next() 메서드로 리턴해서 넘겨주고 반대로 조건의 반대인 경우 로그인 endpoint로 넘깁니다.
    
      - `publicOnlyMiddleware`를 선언합니다. 이번에는 조건 분기시 !를 처음 조건이 없다는것이 참이라는 로직 구성으로 시작하고 그럴경우 next()로 반환처리하고 아닐 경우 루트 URL로 리다이렉트 시킵니다.
    
    2. rootRouter에서는 "/join", "/login" URL에 정의한 미들웨어를 사용합니다. 회원가입에는 publicMiddleware를 사용하는데 이는 all()메서드의 매개변수로 받게합니다. 동일하게 login역시도 위와 같은 로직으로 처리합니다.
    
    3. "/logout", "/edit" endpoint는 로그아웃하거나 말그대로 회원정보를 변경하는 만큼 인증 및 인가 과정이 필요하기에 protectorMiddleware를 사용합니다.
    - logout URL의 경우에는 get()메서드 하나를 이용해서 2번째 자리에 미들웨어를 위치시키며.
    - "/edit"의 경우 get, post요청을 서버가 받기에 route()메서드로 문자열 엔드포인트를 받고 다음 all()메서드로 미들웨어를 받은 다음 get과 post메서드로 각각 컨트롤러함수를 매개변수로 취하게됩니다.
    - 나머지 github로그인을 위한 endpoint들은 모두 publicOnlyMiddleware를 2번쨰 매개변수로 받아 처리하도록합니다.

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,21 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Youtube"
     res.locals.loggedInUser = req.session.user || {};
-
     next();  
-}
+};
+
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+        return next();
+    }else{
+        return res.redirect("/login");
+    }
+};
+
+export const publicMiddleware = (req, res, next) =>{
+    if (!req.session.loggedIn) {
+        return next();
+    }else {
+        return res.redirect("/");
+    }
+};
